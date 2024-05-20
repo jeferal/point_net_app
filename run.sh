c35003ad722b5f870270b6ad55b150dd2b06cedf
Jesus Ferrandiz <jesus.ferrandiz@keybotic.com>
2024-05-21 00:06:11 +0200

    checked if container exists

--- run.sh
+++ run.sh
@@ -2,7 +2,14 @@
 
 # Argument to develop
 if [ "$1" == "--dev" ]; then
-  docker run -it --name point_net_suite --gpus all --net host --privileged --rm \
+
+  # Check if the container exists
+  if [ "$(docker ps -q -f name=point_net_suite)" ]; then
+    docker exec -it point_net_suite /bin/bash
+    exit
+  fi
+
+  docker run -it --name point_net_suite --gpus all --net host --privileged \
     -v data:/data -v $(pwd):/workspace --entrypoint /bin/bash point_net_suite:latest
   exit
 fi
